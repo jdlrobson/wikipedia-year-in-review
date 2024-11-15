893655d78eb7efcc8c0e1265d41df1849d78f894
Jon Robson <jdlrobson@gmail.com>
2024-11-15 11:11:00 -0800

    Format large numbers

--- src/StatBox.vue
+++ src/StatBox.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="statbox">
     <cdx-icon :icon="icon"></cdx-icon>
-    <span>{{ value }}</span>
+    <span>{{ valueFormatted }}</span>
     <span class="statbox-label">{{ label }}</span>
 </div>
 </template>
@@ -11,6 +11,20 @@ export default {
     name: 'StatBox',
     components: {
         CdxIcon
+    },
+    computed: {
+        valueFormatted() {
+            let valString = `${this.value}`;
+            if ( valString.length === 7 ) {
+                return `${ valString.slice( 0, 1 ) },${ valString.slice( 1, 4 ) },${ valString.slice( 4 ) }`;
+            } else if ( valString.length === 6 ) {
+                return `${ valString.slice( 0, 3 ) },${ valString.slice( 3 ) }`;
+            } else if ( valString.length === 5 ) {
+                return `${ valString.slice( 0, 2 ) },${ valString.slice( 2 ) }`;
+            } else {
+                return valString;
+            }
+        }
     },
     props: {
         icon: String|Object,
