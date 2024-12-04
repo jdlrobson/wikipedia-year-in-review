d153648930f9fe89da26989bbfe2ffbfabca46cb
Jon Robson <jdlrobson@gmail.com>
2024-12-04 14:15:11 -0800

    Remove incorrectly committed language conversion code

--- generateLanguages.mjs
+++ generateLanguages.mjs
@@ -1,10 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import langConversions from './langConversions.json' assert { type: "json" };
 import { fileURLToPath } from 'url';
-import lang from './i18n/he.json' assert { type: "json" };
-import en from './i18n/en.json' assert { type: "json" };
-import qqq from './i18n/qqq.json' assert { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
@@ -20,22 +16,4 @@ files.forEach( ( file ) => {
     }
 });
 
-fs.writeFileSync( DESTINATION_FILE, JSON.stringify( supported.sort() ) );
-
-/*
-   # extra $2 param in num-articles
-    */
-Object.keys( langConversions ).forEach( ( newFinalKey ) => {
-    const msg = langConversions[newFinalKey];
-    const newMsg = msg.replace(/«([^«])*»/g, function ( match ) {
-        const key = match.replace(/[«»]/g, '' );
-        if ( [ '«num-articles»', '«edited-most-day-edit-total»' ].indexOf(
-            match
-        ) > -1 ) {
-            return ( lang[key] || en[key] ).replace( '$1', '$2' );
-        } else {
-           return ( lang[key] || en[key] );
-        }
-    } );
-    console.log( `"${newFinalKey}": "${newMsg}",` );
-} );
+fs.writeFileSync( DESTINATION_FILE, JSON.stringify( supported ) );
